e658a795d039a2b746b254627c545ae232e21637
Ajdin Bukvic <ajdinnajdin001@gmail.com>
2022-11-24 15:19:51 +0100

    Initial commit

---
+++ src/main/java/com/ptf/si/wp/zadaca1/SistemZaPracenjeLokalnihDogadjajaApplication.java
@@ -0,0 +1,13 @@
+package com.ptf.si.wp.zadaca1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemZaPracenjeLokalnihDogadjajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemZaPracenjeLokalnihDogadjajaApplication.class, args);
+	}
+
+}
